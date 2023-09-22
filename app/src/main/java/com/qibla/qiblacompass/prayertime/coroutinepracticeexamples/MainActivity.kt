f0b52122f023c1bd65c144863df8c24bb32fbd6e
SardarkhanStella <skhan@stellatechnology.com>
2023-09-22 15:42:22 +0500

    un comment code.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/MainActivity.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/MainActivity.kt
@@ -10,10 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        simpleCoroutine()
-//        main()
-//        launchCoroutines()
+        simpleCoroutine()
+        main()
+        launchCoroutines()
         dependentCoroutines()
+        jobHeirarchy()
     }
 
     fun dependentCoroutines() {
@@ -58,6 +59,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
         Thread.sleep(1000)
+    }
+
+    fun jobHeirarchy() {
+        with(GlobalScope) {
+            val parentJob = launch {
+                delay(200)
+                println("I’m the parent")
+                delay(200)
+            }
+            launch(context = parentJob) {
+                delay(200)
+                println("I’m a child")
+                delay(200)
+            }
+            if (parentJob.children.iterator().hasNext()) {
+                println("The Job has children ${parentJob.children}")
+            } else {
+                println("The Job has NO children")
+            }
+            Thread.sleep(1000)
+        }
     }
 
 
