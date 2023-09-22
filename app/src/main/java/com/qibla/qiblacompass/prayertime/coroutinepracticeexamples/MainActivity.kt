9b5140d8973e015ae15912c65f327b12d21e286c
SardarkhanStella <skhan@stellatechnology.com>
2023-09-22 15:19:56 +0500

    Add simple launch coroutines to the code.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/MainActivity.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/MainActivity.kt
@@ -2,10 +2,37 @@ package com.qibla.qiblacompass.prayertime.coroutinepracticeexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main()
+        launchCoroutines()
     }
+
+    fun main() = runBlocking {
+        val job = launch {
+            delay(1000)
+            Log.d(MainActivity::class.simpleName, "main: Hello From Coroutine...")
+        }
+        job.join()
+    }
+
+    fun launchCoroutines() {
+        (1..10000).forEach {
+            GlobalScope.launch {
+                val threadName = Thread.currentThread().name
+                println("$it printed on thread ${threadName}")
+            }
+        }
+        Thread.sleep(1000)
+    }
+
+
 }
