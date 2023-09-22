c9033bb0f13cd2a2b9913263347799878293ba00
SardarkhanStella <skhan@stellatechnology.com>
2023-09-22 15:29:36 +0500

    Add dependent Coroutine.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/MainActivity.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/MainActivity.kt
@@ -3,18 +3,33 @@ package com.qibla.qiblacompass.prayertime.coroutinepracticeexamples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        simpleCoroutine()
-        main()
-        launchCoroutines()
+
+//        simpleCoroutine()
+//        main()
+//        launchCoroutines()
+        dependentCoroutines()
+    }
+
+    fun dependentCoroutines() {
+        val job1 = GlobalScope.launch(start = CoroutineStart.LAZY) {
+            delay(200)
+            println("Pong")
+            delay(200)
+        }
+        GlobalScope.launch {
+            delay(200)
+            println("Ping")
+            job1.join()
+            println("Ping")
+            delay(200)
+        }
+        Thread.sleep(1000)
     }
 
     private fun simpleCoroutine() {
