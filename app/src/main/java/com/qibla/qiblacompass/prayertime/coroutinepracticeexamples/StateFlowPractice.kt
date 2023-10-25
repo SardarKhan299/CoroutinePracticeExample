647acd12c4af397da2c9ab030eed81658ea1fce4
SardarkhanStella <skhan@stellatechnology.com>
2023-10-25 18:53:50 +0500

    Add State flow and shared flow practice.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/StateFlowPractice.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/coroutinepracticeexamples/StateFlowPractice.kt
@@ -6,32 +6,31 @@ import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-class StateFlow : AppCompatActivity() {
+class StateFlowPractice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_state_flow)
         GlobalScope.launch (Dispatchers.IO){
             val result = producer()
-            delay(8000)
+            Log.d(StateFlowPractice::class.simpleName, "onCreate: ${result.value}")
             result.collect{
-                Log.d(StateFlow::class.simpleName, "onCreate: Collect $it")
+                Log.d(StateFlowPractice::class.simpleName, "onCreate: Collect $it")
             }
         }
 
     }
 
-    private fun producer(): Flow<Int> {
+    private fun producer(): StateFlow<Int> {
         val mutableStateFlow = MutableStateFlow(10)
         GlobalScope.launch {
             val list = listOf<Int>(1,2,3)
             list.forEach{
-                Log.d(StateFlow::class.simpleName, "producer: Emiting $it")
+                Log.d(StateFlowPractice::class.simpleName, "producer: Emiting $it")
                 delay(2000)
                 mutableStateFlow.emit(it)
             }
